f2b3fa3b52c86eaa063690e8fd01fcf4c00d8570
wozniak04 <mikimen321@gmail.com>
2023-04-15 18:36:02 +0200

    dobra wysyłam to co zrobiłem jak narazie już prawie koniec

---
+++ Aplikacja/Aplikacja/UlubionaMuzyka.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Aplikacja
+{
+    partial class UlubionaMuzyka
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImgWykonawca = new System.Windows.Forms.PictureBox();
+            this.Wykonawca = new System.Windows.Forms.Label();
+            this.Tytul = new System.Windows.Forms.Label();
+            this.imgPlay = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgWykonawca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImgWykonawca
+            // 
+            this.ImgWykonawca.Location = new System.Drawing.Point(3, 3);
+            this.ImgWykonawca.Name = "ImgWykonawca";
+            this.ImgWykonawca.Size = new System.Drawing.Size(52, 74);
+            this.ImgWykonawca.TabIndex = 0;
+            this.ImgWykonawca.TabStop = false;
+            // 
+            // Wykonawca
+            // 
+            this.Wykonawca.AutoSize = true;
+            this.Wykonawca.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Wykonawca.Location = new System.Drawing.Point(61, 13);
+            this.Wykonawca.Name = "Wykonawca";
+            this.Wykonawca.Size = new System.Drawing.Size(15, 13);
+            this.Wykonawca.TabIndex = 1;
+            this.Wykonawca.Text = "l1";
+            // 
+            // Tytul
+            // 
+            this.Tytul.AutoSize = true;
+            this.Tytul.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tytul.Location = new System.Drawing.Point(61, 54);
+            this.Tytul.Name = "Tytul";
+            this.Tytul.Size = new System.Drawing.Size(15, 13);
+            this.Tytul.TabIndex = 2;
+            this.Tytul.Text = "l2";
+            // 
+            // imgPlay
+            // 
+            this.imgPlay.BackgroundImage = global::Aplikacja.Properties.Resources.button11;
+            this.imgPlay.InitialImage = null;
+            this.imgPlay.Location = new System.Drawing.Point(246, 13);
+            this.imgPlay.Name = "imgPlay";
+            this.imgPlay.Size = new System.Drawing.Size(40, 40);
+            this.imgPlay.TabIndex = 3;
+            this.imgPlay.TabStop = false; 
+            // 
+            // Muzyki
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.imgPlay);
+            this.Controls.Add(this.Tytul);
+            this.Controls.Add(this.Wykonawca);
+            this.Controls.Add(this.ImgWykonawca);
+            this.Name = "Muzyki";
+            this.Size = new System.Drawing.Size(300, 80);
+            this.Load += new System.EventHandler(this.Muzyki_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ImgWykonawca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImgWykonawca;
+        private System.Windows.Forms.Label Wykonawca;
+        private System.Windows.Forms.Label Tytul;
+        private System.Windows.Forms.PictureBox imgPlay;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Aplikacja/Aplikacja/UlubionaMuzyka.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplikacja
+{
+    public partial class UlubionaMuzyka : UserControl
+    {
+        string Wykonawcas, Tytuls, Email;
+        Image img;
+
+        public event EventHandler<string> Clicked;
+
+        public UlubionaMuzyka(string wykonawca, string tytul, Image image, string user)
+        {
+            Wykonawcas = wykonawca;
+            Tytuls = tytul;
+            img = image;
+            Email = user;
+            InitializeComponent();
+
+            this.imgPlay.Click += new EventHandler(Muzyki_Click);
+        }
+
+
+        private void Muzyki_Load(object sender, EventArgs e)
+        {
+            this.ImgWykonawca.Image = img;
+            this.Wykonawca.Text = Wykonawcas;
+            this.Tytul.Text = Tytuls;
+        }
+
+        private void Muzyki_Click(object sender, EventArgs e)
+        {
+            string tytul = Tytuls + "_" + Wykonawcas + ".wav";
+            Clicked?.Invoke(this, tytul);
+        }
+    }
+}
+
